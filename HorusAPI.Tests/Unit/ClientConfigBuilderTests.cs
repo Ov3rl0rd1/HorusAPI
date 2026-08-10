3c0bd192cf665f2e6510a2a2d9db888b05b3265b
Overlord <nrbudanov1123@gmail.com>
2026-08-10 19:11:33 +0300

    Fix test

--- HorusAPI.Tests/Unit/ClientConfigBuilderTests.cs
+++ HorusAPI.Tests/Unit/ClientConfigBuilderTests.cs
@@ -8,6 +8,7 @@ public class ClientConfigBuilderTests
     private static ServerRow Server() => new(
         server_id:            1,
         host:                "node1.example.com",
+        auth_password:       "authpass",
         reality_public_key:  "PUBKEY123",
         reality_short_ids:   ["aa", "bb"],
         reality_server_name: "www.microsoft.com",
